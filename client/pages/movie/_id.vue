b1abbe4e8a2442539429b53ffa0c71440373c69a
Jose Torreblanca <josewhitetower@gmail.com>
2020-02-15 21:39:14 +0100

    added apollo-query component

--- client/pages/movie/_id.vue
+++ client/pages/movie/_id.vue
@@ -1,27 +1,37 @@
 <template>
-  <div v-if="$apollo.loading">Loading...</div>
+  <!-- <div v-if="$apollo.loading">Loading...</div>
   <div v-else-if="movie">
     <div>{{ movie.title }}</div>
     <img :src="movie.poster_path" alt="" />
-  </div>
+  </div> -->
+  <ApolloQuery
+    :query="require('@/queries/queries').movieQuery"
+    :variables="{ id: $route.params.id }"
+  >
+    <template v-slot="{ result: { loading, error, data } }">
+      <div v-if="loading">Loading...</div>
+      <div v-else-if="data">
+        <div>{{ data.movie.title }}</div>
+        <img :src="data.movie.poster_path" alt="" />
+      </div>
+      <div v-else>No result</div>
+    </template>
+  </ApolloQuery>
 </template>
 
 <script>
-import { movieQuery } from '@/queries/queries'
+// import { movieQuery } from '@/queries/queries'
 export default {
-  data: () => ({
-    movie: null
-  }),
-  apollo: {
-    movie: {
-      query: movieQuery,
-      variables() {
-        return {
-          id: this.$route.params.id
-        }
-      }
-    }
-  }
+  // apollo: {
+  //   movie: {
+  //     query: movieQuery,
+  //     variables() {
+  //       return {
+  //         id: this.$route.params.id
+  //       }
+  //     }
+  //   }
+  // }
 }
 </script>
 
