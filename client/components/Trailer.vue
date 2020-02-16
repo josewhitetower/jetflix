ccac7426859199864e83b86d83a816ccff83b71e
Jose Torreblanca <josewhitetower@gmail.com>
2020-02-16 13:13:51 +0100

    fixed iframe video

--- client/components/Trailer.vue
+++ client/components/Trailer.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="h-full fixed inset-0 w-full z-10 bg-transparent">
-    <div class="relative h-auto">
+    <div class="h-full pb-64 relative">
       <button @click="$emit('close')">close</button>
       <div v-if="loading">Loading...</div>
 
       <iframe
         @load="loading = false"
-        width="640"
-        height="320"
         class="mt-20 mx-auto w-full lg:w-8/12 h-full py-20"
         :src="trailerUrl"
-      >
-      </iframe>
+      ></iframe>
     </div>
   </div>
 </template>
