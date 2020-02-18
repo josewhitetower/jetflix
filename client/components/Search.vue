e19e5e13493d43b99679bf3ced48bfb3d71666cb
Jose Torreblanca <josewhitetower@gmail.com>
2020-02-18 20:01:43 +0100

    added close search

--- client/components/Search.vue
+++ client/components/Search.vue
@@ -1,11 +1,18 @@
 <template>
-  <form @submit.prevent="handleSubmit" class="my-12">
+  <form @submit.prevent="handleSubmit" class="my-12 relative">
     <input
       v-model="query"
       type="text"
       class="block rounded p-2 w-full bg-gray-900"
       placeholder="Search movies.."
     />
+    <nuxt-link
+      to="/"
+      v-if="query"
+      class="absolute right-0 top-0 mt-2 mr-3 cursor-pointer text-lg"
+    >
+      <font-awesome-icon :icon="['fas', 'times']" />
+    </nuxt-link>
   </form>
 </template>
 
