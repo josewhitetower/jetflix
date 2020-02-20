395a57c0ce622a4c956ea96a54d35a6590ae8e0e
Jose Torreblanca <josewhitetower@gmail.com>
2020-02-20 23:59:24 +0100

    deployed backend

--- server/app.js
+++ server/app.js
@@ -3,10 +3,10 @@ const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
 const cors = require('cors')
 require('dotenv').config()
-
+const port = process.env.PORT || 4000
 const app = express()
 // app.use(cors())
 app.use('/graphql', cors(), graphqlHTTP({ schema, graphiql: true }))
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`)
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
 })
