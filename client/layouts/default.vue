d247c3e85118283efd64d9803ae2a2aa87dbde1e
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2020-02-24 11:21:41 +0100

    added footer

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="container mx-auto px-4 mt-24 pb-8">
+  <div class="container mx-auto px-4 mt-24 pb-8 relative pb-12">
     <Navigation />
     <Search />
     <nuxt />
+    <footer class="absolute bottom-0 py-4 text-center text-sm w-full">
+      Powered by
+      <a
+        class="underline italic"
+        href="https://www.themoviedb.org/"
+        rel="nofollow"
+        target="_blank"
+        >The Movie Database</a
+      >
+    </footer>
   </div>
 </template>
 
