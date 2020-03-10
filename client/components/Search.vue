841c2b67d9eed6c104492590dd079bbc7f918207
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2020-03-10 22:48:06 +0100

    added label to search

--- client/components/Search.vue
+++ client/components/Search.vue
@@ -2,16 +2,23 @@
   <form class="my-12 relative" @submit.prevent="handleSubmit">
     <!-- using @input instead of v-model because on mobile the watcher on query
     is not detected see: https://github.com/vuejs/vue/issues/8231 -->
+    <label
+      for="search"
+      class="mb-2 px-2 transform inline-block transition duration-75 ease-in-out"
+      :class="[query ? 'opacity-100' : 'opacity-0']"
+      >Search movies...</label
+    >
     <input
-      :value="query"
+      id="search"
       type="text"
       class="block rounded p-2 w-full bg-gray-900"
       placeholder="Search movies..."
+      :value="query"
       @input="(e) => (query = e.target.value)"
     />
     <font-awesome-icon
       v-if="query"
-      class="absolute right-0 top-0 mt-3 mr-3 cursor-pointer text-lg"
+      class="absolute right-0 bottom-0 mb-3 mr-3 cursor-pointer text-lg"
       :icon="['fas', 'times']"
       @click="query = ''"
     />
