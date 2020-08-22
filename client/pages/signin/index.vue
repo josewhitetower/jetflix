466f5c94c4268d30056873547c4c8e5443c0dc3a
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2020-08-22 23:41:04 +0200

    added error message

--- client/pages/signin/index.vue
+++ client/pages/signin/index.vue
@@ -1,38 +1,43 @@
 <template>
-  <div class="flex justify-center mb-40">
-    <form
-      @submit.prevent="signInWithEmailAndPassword"
-      class="border border-gray-100 rounded py-4 px-8 flex flex-col"
-    >
-      <div class="my-4 flex items-center justify-between">
-        <label class="mr-3" for="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          required
-          placeholder="john@doe.com"
-          v-model="email"
-          class="text-gray-800 rounded p-2"
-        />
-      </div>
-      <div class="my-4 flex items-center justify-between">
-        <label class="mr-3" for="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          required
-          autocomplete="current-password"
-          placeholder="********"
-          v-model="password"
-          class="text-gray-800 rounded p-2"
-        />
-      </div>
-      <button
-        class="mt-4 px-2 py-3 border border-gray-100 rounded hover:bg-gray-900"
+  <div class="mb-40">
+    <div class="flex justify-center">
+      <form
+        @submit.prevent="signInWithEmailAndPassword"
+        class="border border-gray-100 rounded py-4 px-8 flex flex-col"
       >
-        Sign in
-      </button>
-    </form>
+        <div class="my-4 flex items-center justify-between">
+          <label class="mr-3" for="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="john@doe.com"
+            v-model="email"
+            class="text-gray-800 rounded p-2"
+          />
+        </div>
+        <div class="my-4 flex items-center justify-between">
+          <label class="mr-3" for="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            required
+            autocomplete="current-password"
+            placeholder="********"
+            v-model="password"
+            class="text-gray-800 rounded p-2"
+          />
+        </div>
+        <button
+          class="mt-4 px-2 py-3 border border-gray-100 rounded hover:bg-gray-900"
+        >
+          Sign in
+        </button>
+      </form>
+    </div>
+    <p v-if="error" class="text-red-600 mt-4 flex-grow-0 text-center">
+      {{ error }}
+    </p>
   </div>
 </template>
 
@@ -40,7 +45,8 @@
 export default {
   data: () => ({
     email: '',
-    password: ''
+    password: '',
+    error: ''
   }),
   methods: {
     signInWithEmailAndPassword() {
@@ -50,7 +56,7 @@ export default {
           password: this.password
         })
         .then(() => this.$router.push('/'))
-        .catch((e) => console.log(e))
+        .catch((e) => (this.error = e.message))
     }
   }
 }
--- client/pages/signup/index.vue
+++ client/pages/signup/index.vue
@@ -1,49 +1,54 @@
 <template>
-  <div class="flex justify-center mb-40">
-    <form
-      @submit.prevent="createUserWithEmailAndPassword"
-      class="border border-gray-100 rounded py-4 px-8 flex flex-col"
-    >
-      <div class="my-4 flex items-center justify-between">
-        <label class="mr-3" for="displayName">Name</label>
-        <input
-          type="text"
-          name="displayName"
-          required
-          placeholder="John Doe"
-          v-model="displayName"
-          class="text-gray-800 rounded p-2"
-        />
-      </div>
-      <div class="my-4 flex items-center justify-between">
-        <label class="mr-3" for="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          required
-          placeholder="john@doe.com"
-          v-model="email"
-          class="text-gray-800 rounded p-2"
-        />
-      </div>
-      <div class="my-4 flex items-center justify-between">
-        <label class="mr-3" for="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          required
-          autocomplete
-          placeholder="********"
-          v-model="password"
-          class="text-gray-800 rounded p-2"
-        />
-      </div>
-      <button
-        class="mt-4 px-2 py-3 border border-gray-100 rounded hover:bg-gray-900"
+  <div class="mb-40">
+    <div class="flex justify-center">
+      <form
+        @submit.prevent="createUserWithEmailAndPassword"
+        class="border border-gray-100 rounded py-4 px-8 flex flex-col"
       >
-        Sign up
-      </button>
-    </form>
+        <div class="my-4 flex items-center justify-between">
+          <label class="mr-3" for="displayName">Name</label>
+          <input
+            type="text"
+            name="displayName"
+            required
+            placeholder="John Doe"
+            v-model="displayName"
+            class="text-gray-800 rounded p-2"
+          />
+        </div>
+        <div class="my-4 flex items-center justify-between">
+          <label class="mr-3" for="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="john@doe.com"
+            v-model="email"
+            class="text-gray-800 rounded p-2"
+          />
+        </div>
+        <div class="my-4 flex items-center justify-between">
+          <label class="mr-3" for="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            required
+            autocomplete
+            placeholder="********"
+            v-model="password"
+            class="text-gray-800 rounded p-2"
+          />
+        </div>
+        <button
+          class="mt-4 px-2 py-3 border border-gray-100 rounded hover:bg-gray-900"
+        >
+          Sign up
+        </button>
+      </form>
+    </div>
+    <p v-if="error" class="text-red-600 mt-4 flex-grow-0 text-center">
+      {{ error }}
+    </p>
   </div>
 </template>
 
@@ -52,7 +57,8 @@ export default {
   data: () => ({
     email: '',
     password: '',
-    displayName: ''
+    displayName: '',
+    error: ''
   }),
   methods: {
     createUserWithEmailAndPassword() {
@@ -63,7 +69,7 @@ export default {
           displayName: this.displayName
         })
         .then(() => this.$router.push('/'))
-        .catch((e) => console.log(e))
+        .catch((e) => (this.error = e.message))
     }
   }
 }
