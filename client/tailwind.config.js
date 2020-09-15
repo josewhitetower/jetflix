35a38386044d1cb91d4196462a72a3cb21330b3c
Torreblanca, Jose <jose.torreblanca@spiegel.de>
2020-09-16 00:04:43 +0200

    updated postccss and target

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -19,5 +19,16 @@ module.exports = {
       }
       addUtilities(newUtilities)
     })
-  ]
+  ],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
+  }
 }
